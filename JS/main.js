85510f7de5a05c73e2af654da9e3374e8a1d837f
RailJannatov <railcennetov@gmail.com>
2021-11-03 11:19:04 +0400

    header-updated

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,22 @@
-// $('.carousel').carousel({
-//      interval: 1000
-// })
-
 $('.content').slick(
-   
-);
+{ 
+ prevArrow: '<i class="fas fa-long-arrow-alt-left prev-style"></i>',
+    nextArrow: '<i class="fas fa-long-arrow-alt-right next-style"></i>',
+    
+  });
+
+$('.test').isotope({
+    itemSelector:".item",
+    layoutMode:"fitRows"
+});
+$('.flower-categories ul li').click(function(){
+    $('.flower-categories ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-filter');
+    $('.test').isotope({
+        filter:selector
+    });
+    return false;
+
+})
