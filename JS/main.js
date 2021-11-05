b6dfb624e53193c48446ea55ac994d9a1d06c4de
RailJannatov <railcennetov@gmail.com>
2021-11-06 03:26:15 +0400

    standard-list-page-done and navbar updated

--- JS/main.js
+++ JS/main.js
@@ -1,66 +1,92 @@
-$('.content').slick(
-{ 
- prevArrow: '<i class="fas fa-long-arrow-alt-left prev-style"></i>',
+$('.content').slick({
+    prevArrow: '<i class="fas fa-long-arrow-alt-left prev-style"></i>',
     nextArrow: '<i class="fas fa-long-arrow-alt-right next-style"></i>',
-    
-  });
+
+});
 
 $('.test').isotope({
-    itemSelector:".item",
-    layoutMode:"fitRows"
+    itemSelector: ".item",
+    layoutMode: "fitRows"
 });
-$('.flower-categories ul li').click(function(){
+$('.flower-categories ul li').click(function () {
     $('.flower-categories ul li').removeClass('active');
     $(this).addClass('active');
-    
+
     var selector = $(this).attr('data-filter');
     $('.test').isotope({
-        filter:selector
+        filter: selector
     });
     return false;
 
 })
 
 $('.options').isotope({
-    itemSelector:".item",
-    layoutMode:"fitRows"
+    itemSelector: ".item",
+    layoutMode: "fitRows"
 });
-$('.flower-options-categories ul li').click(function(){
+$('.flower-options-categories ul li').click(function () {
     $('.flower-options-categories ul li').removeClass('active');
     $(this).addClass('active');
-    
+
     var selector = $(this).attr('data-filter');
     $('.options').isotope({
-        filter:selector
+        filter: selector
     });
     return false;
 
 })
 
-$(document).ready(function(){
-    $(".element").click(function(){
-     console.log( $(".element-context").not($(this).next()).slideUp(1000))  
+$(document).ready(function () {
+    $(".element").click(function () {
+        console.log($(".element-context").not($(this).next()).slideUp(1000))
         $(this).next().slideToggle(1000);
         $(".active").removeClass("active")
         $(this).addClass("active");
     })
-})
-let allBodyTitle =document.querySelectorAll(".tab-menu  .content-title");
-let allBodyItems=document.querySelectorAll(".tab-menu .content-body .item")
+    $('.gallerys').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
 
-allBodyTitle.forEach(title=>{
-    title.addEventListener("click",function(){
-        let active =document.querySelector(".active");
+    })
+    console.log($('button .mfp arrow'));
+
+})
+
+
+let allBodyTitle = document.querySelectorAll(".tab-menu  .content-title");
+let allBodyItems = document.querySelectorAll(".tab-menu .content-body .item")
+
+let addBtn = document.querySelector(".addBtn");
+let removeBtn = document.querySelector(".removeBtn");
+const quantityNumber = document.querySelector(".quantityNumber");
+
+
+allBodyTitle.forEach(title => {
+    title.addEventListener("click", function () {
+        let active = document.querySelector(".active");
         active.classList.remove("active");
         this.classList.add("active");
         let id = title.getAttribute("data-id");
-        allBodyItems.forEach(item=>{
-            if(item.getAttribute("data-id") ==id){
-               item.classList.remove("d-none")
-            }else{
+        allBodyItems.forEach(item => {
+            if (item.getAttribute("data-id") == id) {
+                item.classList.remove("d-none")
+            } else {
                 item.classList.add("d-none")
             }
         })
 
     })
+})
+
+addBtn.addEventListener("click", function (e) {
+    quantityNumber.innerText++;
+})
+removeBtn.addEventListener("click", function (e) {
+    if (quantityNumber.innerText <= 0) {
+        return alert("Item doesn't have");
+    }
+    quantityNumber.innerText--;
 })
