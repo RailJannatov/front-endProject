20c08a980e86ab06760dab9632eb2bf5b2025a74
RailJannatov <railcennetov@gmail.com>
2021-11-05 15:58:04 +0400

    product-list-tabs-section-done

--- JS/main.js
+++ JS/main.js
@@ -45,3 +45,22 @@ $(document).ready(function(){
         $(this).addClass("active");
     })
 })
+let allBodyTitle =document.querySelectorAll(".tab-menu  .content-title");
+let allBodyItems=document.querySelectorAll(".tab-menu .content-body .item")
+
+allBodyTitle.forEach(title=>{
+    title.addEventListener("click",function(){
+        let active =document.querySelector(".active");
+        active.classList.remove("active");
+        this.classList.add("active");
+        let id = title.getAttribute("data-id");
+        allBodyItems.forEach(item=>{
+            if(item.getAttribute("data-id") ==id){
+               item.classList.remove("d-none")
+            }else{
+                item.classList.add("d-none")
+            }
+        })
+
+    })
+})
