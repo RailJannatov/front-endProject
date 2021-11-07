75e7015b53ce90407b659f9693688b6a9a558fa5
RailJannatov <railcennetov@gmail.com>
2021-11-07 22:38:18 +0400

    basket-done

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,92 @@
+// basket function
+
+
+let popup = document.querySelector(".popup");
+
+let modalBasket = document.querySelector(".modalBasket");
+
+let iconClose = document.querySelector(".popup i");
+
+iconClose.addEventListener("click", function (e) {
+    popup.classList.add("d-none");
+})
+window.addEventListener("click", function (e) {
+    if (e.target === popup) {
+        popup.classList.add("d-none");
+    }
+});
+
+
+
+
+let itemsFlower = document.querySelectorAll('#flower-items .item');
+const items = [];
+itemsFlower.forEach((item, idx) =>
+
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        popup.classList.remove("d-none");
+        let item = {
+            id: idx + 1,
+            no: 1,
+        };
+        let name = this.children[0].nextElementSibling.children[0].children[0].textContent;
+        let price = this.children[0].nextElementSibling.children[1].children[1].textContent;
+        let customizedPrice = price.slice(1);
+        item.name = name;
+        item.customizedPrice = customizedPrice;
+        if (JSON.parse(localStorage.getItem('items')) === null) {
+            items.push(item);
+            localStorage.setItem('items', JSON.stringify(items));
+        } else {
+            const localItems = JSON.parse(localStorage.getItem('items'));
+            window.location.reload();
+            localItems.map(data => {
+                if (item.id == data.id) {
+                    item.no = data.no + 1
+                } else {
+                    items.push(data);
+                }
+            });
+            items.push(item);
+            localStorage.setItem('items', JSON.stringify(items));
+            window.location.reload();
+        }
+
+    }));
+let itemsInBag = document.querySelector('.shopping-bag-icon .items-in-bag');
+let numberInBag = 0;
+JSON.parse(localStorage.getItem('items')).map(data => {
+    numberInBag = numberInBag + data.no;
+})
+itemsInBag.innerHTML = numberInBag;
+
+
+let cartBasket = document.querySelector('.cart-basket');
+let row = document.createElement('li');
+row = '';
+cartBasket.append(row);
+if (JSON.parse(localStorage.getItem('items'))[0] === null) {
+    row += `Bele bir product yoxdur`
+} else {
+    JSON.parse(localStorage.getItem('items')).map(data => {
+        row += `Product id:${data.id }  +Product no: ${data.no} +  Product name :${data.name} + product price :${data.customizedPrice}`;
+    })
+}
+cartBasket.innerHTML = row;
+
+
+console.log(cartBasket);
+
+// mini-slider function 
 $('.content').slick({
     prevArrow: '<i class="fas fa-long-arrow-alt-left prev-style"></i>',
     nextArrow: '<i class="fas fa-long-arrow-alt-right next-style"></i>',
 
 });
 
+
+// filter items function
 $('.test').isotope({
     itemSelector: ".item",
     layoutMode: "fitRows"
@@ -36,9 +119,35 @@ $('.flower-options-categories ul li').click(function () {
 
 })
 
+
+// scrolling header function
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 370) {
+            $('header .nav-list').removeClass('fixed-header')
+            $('.main-nav').removeClass('fixed')
+            $('.main-logo').removeClass('fixed-logo')
+            $('.search-icon').css('visibility', 'visible')
+            $('.cart-info').css('visibility', 'visible')
+            $('.shopping-bag-icon').css('visibility', 'visible')
+        } else {
+            $('header .nav-list').addClass('fixed-header')
+            $('.main-nav').addClass('fixed')
+            $('.main-logo').addClass('fixed-logo')
+            $('.search-icon').css('visibility', 'hidden')
+            $('.cart-info').css('visibility', 'hidden')
+            $('.shopping-bag-icon').css('visibility', 'hidden')
+        }
+    })
+})
+
+
+
+
+// accordion function
 $(document).ready(function () {
     $(".element").click(function () {
-        console.log($(".element-context").not($(this).next()).slideUp(1000))
+        $(".element-context").not($(this).next()).slideUp(1000)
         $(this).next().slideToggle(1000);
         $(".active").removeClass("active")
         $(this).addClass("active");
@@ -54,6 +163,9 @@ $(document).ready(function () {
 
 
 })
+
+
+// right bottom button scroll to top function
 $(document).ready(function () {
     $(window).scroll(function () {
         if ($(window).scrollTop() > 273) {
@@ -69,6 +181,7 @@ $(document).ready(function () {
     });
 })
 
+// tabs and counter function
 
 let allBodyTitle = document.querySelectorAll(".tab-menu  .content-title");
 let allBodyItems = document.querySelectorAll(".tab-menu .content-body .item")
@@ -98,9 +211,12 @@ allBodyTitle.forEach(title => {
 addBtn.addEventListener("click", function (e) {
     quantityNumber.innerText++;
 })
+
+
 removeBtn.addEventListener("click", function (e) {
     if (quantityNumber.innerText <= 0) {
-        return alert("Item doesn't have");
+        alert("there is no negative number of products");
+    } else {
+        quantityNumber.innerText--;
     }
-    quantityNumber.innerText--;
 })
